9e9c4bca3223482e9117f75dcc9c01c8452b2633
bielsworldx <123701715+bielsworldx@users.noreply.github.com>
2023-01-31 20:29:50 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+//crie uma matriz que armazena o peso de 4 pessoas
+var peso = [15, 22, 31, 37];
+
+//crie uma função para calcular a média dos pesos e console o resultado
+function calculate(){
+  var soma = peso[0] + peso[1] + peso[2] + peso [3];
+  var media = soma/peso.lenght;
+  console.log (media); 
+}
+
+
+
+
+function setup() 
+{
+  createCanvas(400,400);
+  //chame a função criada para calcular a média dos pesos
+  calculate();
+}
+
+function draw() 
+{
+background(51);
+
+}
+
